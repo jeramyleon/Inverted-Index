bac5f10dc858edcc76a64196e12cd1de3d4801d1
jeramyleon <jeramyleon@gmail.com>
2022-05-12 19:37:51 -0400

    progress

--- program.py
+++ program.py
@@ -1,9 +1,47 @@
-class Inverted_index:
-    def __init__(self, file_list):
-        self.dictionary = {}
+import nltk 
+import re
+
+# Functions for cleaning up lines in files 
+def remove_stopwords(line):
+    words = line.split()
+    stopwords = nltk.corpus.stopwords.words('english')
+    new_words = [word for word in words if word not in stopwords]
+    return new_words 
+
+def remove_punc(line): 
+    new_words = re.sub(r'[^\w\s]', '', line)
+    return new_words
 
 
-
+# Actual program
 file_list = ['sample3.txt']
-new_index = Inverted_index()
+inverted_index = {}
+
+
+# Prints a list of lists 
+for file in file_list:
+    words = []
+    open_file = open(file, 'r', encoding='utf8')
+    
+    for line in open_file:
+        clean_line = remove_stopwords(remove_punc(line.lower()))
+        words.append(clean_line)
+
+    print(words)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
