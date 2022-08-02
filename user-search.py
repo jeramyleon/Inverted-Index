0dab858b70b7a6778d04b34ffa2754130b7d9c73
jeramyleon <jeramyleon@gmail.com>
2022-08-01 22:22:39 -0400

    new

--- user-search.py
+++ user-search.py
@@ -1,15 +1,19 @@
 import dearpygui.dearpygui as dpg 
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=300)
+dpg.create_viewport(title='Joogle', width=600, height=300)
 
-with dpg.window(label="Example Window"):
-    dpg.add_button(label='search')
-    dpg.add_input_text(label='string', default_value='enter text')
+def button_callback(): 
+    dpg.add_text('Term appears in files...')
 
+with dpg.window(tag="Primary Window"):
+    dpg.add_input_text(default_value='enter text')
+    dpg.add_button(label='search', callback=button_callback)
 
+    
 dpg.setup_dearpygui()
 dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
 
