44ffb948d1b1f76152e2c4f6f5242b0c876e2bed
jeramyleon <jeramyleon@gmail.com>
2022-08-09 22:41:41 -0400

    working on searches

--- user-search.py
+++ user-search.py
@@ -8,13 +8,18 @@ id_filenames shows the id numbers that point to the files
 
 dpg.create_context()
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello, World")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="String", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+with dpg.window(tag="Primary Window"):
+    dpg.add_text("Joogle")
+    dpg.add_input_text(default_value="search here")
+    dpg.add_button(label="search")
+dpg.create_viewport(title="Custom Title", width=600, height=200)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
 
-dpg.create_viewport(title="Custom Title", )
+
 
 
 
